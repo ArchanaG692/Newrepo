fdad7556357f06d6edd9712c371e46a732daab5b
ArchanaG692 <gogeneni.archana98@gmail.com>
2025-06-26 15:42:47 -0400

    V5_107

--- force-app/main/default/lwc/demo1.js
+++ force-app/main/default/lwc/demo1.js
@@ -1,6 +1,6 @@
 export default class TestComp {
   connectedCallback() {
     const unusedVar = 'test'; // ESLint: no-unused-vars
-    console.log('Hell1o111111'); // ESLint: no-console
+    console.log('Hell1o1111111'); // ESLint: no-console
   }
 }
